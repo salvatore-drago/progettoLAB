619e7262a3b73ea6b9d33f1a8a08787205e82f8a
salvatore-drago <salvatore.drago06@community.unipa.it>
2022-03-17 15:19:11 +0100

    Parametrizzare le funzioni, metriche terminate

--- funzione_di_rilevamento.py
+++ funzione_di_rilevamento.py
@@ -6,7 +6,6 @@ T= 20 #voglio analizzare i dati ogni 20s
 N= 50 #voglio conservare i dati relativi alle ultime 50 persone 
 k= 0.6 # k può assumere valori [0;1] e permette di  assegnare piu o meno importanza alla distribuzione delle etichette tra i vari disp/luoghi.
 s_min= 65 # scarta i dati con pr.etichetta inferiore al 65%
-s=85 # considerali molto attendibili
 d_min= 0.1 #cambia in 'SOSPETTO' se sei inferiore a questa soglia
 d_max=0.4 #cambia in 'NON SOSPETTO' se sei superiore a questa soglia
 cs = [0,1,0,1]
